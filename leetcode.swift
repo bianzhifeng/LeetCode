2990cf3c81d4ba67bb595546729818e3ccd75982
SapientiaPeak_N1 <1107733919@qq.com>
2019-11-06 17:56:23 +0800

    旋转数组

--- leetcode.swift
+++ leetcode.swift
@@ -12,4 +12,40 @@ class Solution {
         }
         return next + 1
     }
+    
+    // 189 旋转数组:
+    func rotate_1(_ nums: inout [Int], _ k: Int) {
+        if 1 >= nums.count { return }
+        for i in 0..<k {
+            nums.insert(nums[nums.count - 1], at: 0)
+            nums.removeLast()
+        }
+    }
+        
+    func rotate_2(_ nums: inout [Int], _ k: Int) {
+        if 1 >= nums.count { return }
+        for i in 0..<k {
+            var last = nums[nums.count - 1]
+             for j in 0..<nums.count {
+                var next = nums[j]
+                nums[j] = last
+                last = next
+            }
+        }
+    }
+        
+    func rotate_2(_ nums: inout [Int], _ k: Int) {
+        if 1 >= nums.count { return }
+        var newNums = [Int]()
+        var max = (k > nums.count ? (k - nums.count):k)
+        for i in 0..<max {
+            var last = nums[nums.count - max + i]
+            newNums.append(last)
+            nums.remove(at: nums.count - max + i)
+        }
+        
+        newNums.append(contentsOf: nums)
+        nums = newNums
+    }
+    
 }
